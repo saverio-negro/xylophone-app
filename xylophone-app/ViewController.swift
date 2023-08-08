ae00a096b879c0e68aed6bddfc8f6798ecbbb198
saverio-negro <saverionegro@gmail.com>
2023-08-08 21:36:15 +0200

    Added on changing opacity effect when button gets tapped.

--- xylophone-app/ViewController.swift
+++ xylophone-app/ViewController.swift
@@ -18,6 +18,10 @@ class ViewController: UIViewController {
     }
     
     @IBAction func keyPressed(_ sender: UIButton) {
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1
+        }
         let buttonKey = (sender.titleLabel?.text!)!
         playSound(key: buttonKey)
     }
