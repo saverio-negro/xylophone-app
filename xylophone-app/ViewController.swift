47281ae5fa2e73f5f9863b577f77038be1b2ebf8
saverio-negro <saverionegro@gmail.com>
2023-08-08 20:50:19 +0200

    Added a comment in the ViewController.swift file.

--- xylophone-app/ViewController.swift
+++ xylophone-app/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // This is a comment
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
