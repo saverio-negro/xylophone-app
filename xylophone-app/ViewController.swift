0a925031cf6eb0c045800691274decc65e3527a1
saverio-negro <saverionegro@gmail.com>
2023-08-08 21:32:32 +0200

    Created a new "sound" folder in the "xylophone-app" folder. Also, added "playSound" function to make each of the buttons on the UI play their corresponding key sound.

--- xylophone-app/ViewController.swift
+++ xylophone-app/ViewController.swift
@@ -6,8 +6,11 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,13 +18,22 @@ class ViewController: UIViewController {
     }
     
     @IBAction func keyPressed(_ sender: UIButton) {
-        
-        let buttonKey = sender.currentTitle!
+        let buttonKey = (sender.titleLabel?.text!)!
         playSound(key: buttonKey)
     }
     
     func playSound(key: String) {
         
+        let url = Bundle.main.url(forResource: key, withExtension: "wav")
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            player = try AVAudioPlayer(contentsOf: url!)
+            player?.play()
+        } catch let error {
+            print(error)
+        }
     }
 }
 
